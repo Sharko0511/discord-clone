5c0476c11bfc1168a0a7cf18cf172a9f2c99e201
Sharko0511 <duyquang511@gmail.com>
2024-06-26 07:26:30 +0000

    fix: :bug: fix middleware auth

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"],
+  publicRoutes: ["/", "/api/uploadthing"], // Add "/" to the publicRoutes array
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"], // Ensure the matcher pattern is correct
 };
